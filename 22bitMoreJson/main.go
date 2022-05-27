92a9dbfec22c572bf12d3780b5d15f9d76a6308c
Amimul Ehsan <amimul.ehsan@sayurbox.com>
2022-05-27 22:26:35 +0600

    json consuming done.

--- 22bitMoreJson/main.go
+++ 22bitMoreJson/main.go
@@ -25,7 +25,35 @@ func EncodeJson() {
 	fmt.Printf("%s\n", finalJson)
 }
 
+func DecodeJson() {
+	jsonFromWeb := []byte(`
+		{
+                "coursename": "ReactJs BootCamp",
+                "price": 299,
+                "wesites": "LearnCode.in",
+                "tags": ["web-dev", "js"]
+        }
+	`)
+
+	var lcoCourse course
+
+	checkValid := json.Valid(jsonFromWeb)
+	if checkValid {
+		fmt.Println("Json was valid")
+		json.Unmarshal(jsonFromWeb, &lcoCourse)
+		fmt.Printf("%#v\n", lcoCourse)
+	} else {
+		fmt.Println("Json was not valid")
+	}
+
+	//Don't want to crate model classes each time. Rather give me key value pair
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonFromWeb, &myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData)
+}
+
 func main() {
 	fmt.Println("Welcome to JSON video")
-	EncodeJson()
+	//EncodeJson()
+	DecodeJson()
 }
